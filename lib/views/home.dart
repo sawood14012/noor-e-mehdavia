c5c17130accd5a41bcfc9c6c0be218b0b6725418
Meer Sawood <msawood@redhat.com>
2020-11-01 16:56:39 +0530

    home page done

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -8,6 +8,7 @@ import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:mehdavia/models/timings.dart';
 import 'package:intl/intl.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import 'package:http/http.dart' as http;
 
@@ -56,28 +57,31 @@ class _Home extends State<Home> {
       future: timings,
       builder: (context, snapshot) {
         if (snapshot.hasData) {
-          return Column(
-            // Column is also a layout widget. It takes a list of children and
-            // arranges them vertically. By default, it sizes itself to fit its
-            // children horizontally, and tries to be as tall as its parent.
-            //
-            // Invoke "debug painting" (press "p" in the console, choose the
-            // "Toggle Debug Paint" action from the Flutter Inspector in Android
-            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-            // to see the wireframe for each widget.
-            //
-            // Column has various properties to control how it sizes itself and
-            // how it positions its children. Here we use mainAxisAlignment to
-            // center the children vertically; the main axis here is the vertical
-            // axis because Columns are vertical (the cross axis would be
-            // horizontal).
-            mainAxisAlignment: MainAxisAlignment.start,
+          return ListView(
+            padding: const EdgeInsets.all(8),
             children: <Widget>[
-              Card(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    /*  ListTile(
+            Column(
+              // Column is also a layout widget. It takes a list of children and
+              // arranges them vertically. By default, it sizes itself to fit its
+              // children horizontally, and tries to be as tall as its parent.
+              //
+              // Invoke "debug painting" (press "p" in the console, choose the
+              // "Toggle Debug Paint" action from the Flutter Inspector in Android
+              // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+              // to see the wireframe for each widget.
+              //
+              // Column has various properties to control how it sizes itself and
+              // how it positions its children. Here we use mainAxisAlignment to
+              // center the children vertically; the main axis here is the vertical
+              // axis because Columns are vertical (the cross axis would be
+              // horizontal).
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Card(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      /*  ListTile(
                       leading: Icon(CupertinoIcons.hourglass_bottomhalf_fill),
                       title: Text("Fajr : 5:00 am"),
                       subtitle: Text("15 min's left"),
@@ -86,119 +90,141 @@ class _Home extends State<Home> {
                       height: 10,
                       thickness: 3,
                     ), */
-                    ListTile(
-                      title: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[Text("Today's Timings")]),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Chip(
-                          avatar: CircleAvatar(
-                            backgroundColor: Colors.grey.shade800,
-                            child: Icon(CupertinoIcons.sunrise),
+                      ListTile(
+                        title: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[Text("Today's Timings")]),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Chip(
+                            avatar: CircleAvatar(
+                              backgroundColor: Colors.grey.shade800,
+                              child: Icon(CupertinoIcons.sunrise),
+                            ),
+                            label: Text(convertdate12(snapshot.data.sunrise)),
                           ),
-                          label: Text(convertdate12(snapshot.data.sunrise)),
-                        ),
-                        const SizedBox(width: 8),
-                        Chip(
-                          avatar: CircleAvatar(
-                            backgroundColor: Colors.grey.shade800,
-                            child: Icon(CupertinoIcons.sunset),
+                          const SizedBox(width: 8),
+                          Chip(
+                            avatar: CircleAvatar(
+                              backgroundColor: Colors.grey.shade800,
+                              child: Icon(CupertinoIcons.sunset),
+                            ),
+                            label: Text(convertdate12(snapshot.data.sunset)),
                           ),
-                          label: Text(convertdate12(snapshot.data.sunset)),
-                        ),
-                        const SizedBox(width: 8),
-                        Chip(
-                          avatar: CircleAvatar(
-                            backgroundColor: Colors.grey.shade800,
-                            child: Icon(CupertinoIcons.moon_stars),
+                          const SizedBox(width: 8),
+                          Chip(
+                            avatar: CircleAvatar(
+                              backgroundColor: Colors.grey.shade800,
+                              child: Icon(CupertinoIcons.moon_stars),
+                            ),
+                            label: Text(convertdate12(snapshot.data.midnight)),
+                          )
+                        ],
+                      ),
+                      Divider(
+                        height: 10,
+                        thickness: 3,
+                      ),
+                      ListTile(
+                        title: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[Text("Prayer Timings")]),
+                      ),
+                      DataTable(
+                        columns: const <DataColumn>[
+                          DataColumn(
+                            label: Text(
+                              'Prayer',
+                              style: TextStyle(fontStyle: FontStyle.italic),
+                            ),
                           ),
-                          label: Text(convertdate12(snapshot.data.midnight)),
-                        )
-                      ],
-                    ),
-                    Divider(
-                      height: 10,
-                      thickness: 3,
-                    ),
-                    ListTile(
-                      title: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[Text("Prayer Timings")]),
-                    ),
-                    DataTable(
-                      columns: const <DataColumn>[
-                        DataColumn(
-                          label: Text(
-                            'Prayer',
-                            style: TextStyle(fontStyle: FontStyle.italic),
+                          DataColumn(
+                            label: Text(
+                              'Start Time',
+                              style: TextStyle(fontStyle: FontStyle.italic),
+                            ),
                           ),
-                        ),
-                        DataColumn(
-                          label: Text(
-                            'Start Time',
-                            style: TextStyle(fontStyle: FontStyle.italic),
+                          DataColumn(
+                            label: Text(
+                              'End Time',
+                              style: TextStyle(fontStyle: FontStyle.italic),
+                            ),
                           ),
-                        ),
-                        DataColumn(
-                          label: Text(
-                            'End Time',
-                            style: TextStyle(fontStyle: FontStyle.italic),
+                        ],
+                        rows: <DataRow>[
+                          DataRow(
+                            cells: <DataCell>[
+                              DataCell(Text('Fajr')),
+                              DataCell(Text(convertdate12(snapshot.data.fajr))),
+                              DataCell(Text(convertdate(snapshot.data.fajr))),
+                            ],
                           ),
-                        ),
-                      ],
-                      rows: <DataRow>[
-                        DataRow(
-                          cells: <DataCell>[
-                            DataCell(Text('Fajr')),
-                            DataCell(Text(convertdate12(snapshot.data.fajr))),
-                            DataCell(Text(convertdate(snapshot.data.fajr))),
-                          ],
-                        ),
-                        DataRow(
-                          cells: <DataCell>[
-                            DataCell(Text('Dhur')),
-                            DataCell(Text(convertdate12(snapshot.data.dhur))),
-                            DataCell(Text(convertdate(snapshot.data.dhur))),
-                          ],
-                        ),
-                        DataRow(
-                          cells: <DataCell>[
-                            DataCell(Text('Asr')),
-                            DataCell(Text(convertdate12(snapshot.data.asr))),
-                            DataCell(Text(convertdate(snapshot.data.asr))),
-                          ],
-                        ),
-                        DataRow(
-                          cells: <DataCell>[
-                            DataCell(Text('Maghrib')),
-                            DataCell(Text(convertdate12(snapshot.data.maghrib))),
-                            DataCell(Text(convertdate(snapshot.data.maghrib))),
-                          ],
-                        ),
-                        DataRow(
-                          cells: <DataCell>[
-                            DataCell(Text('Isha')),
-                            DataCell(Text(convertdate12(snapshot.data.isha))),
-                            DataCell(Text(convertdate(snapshot.data.isha))),
-                          ],
-                        ),
-                        DataRow(
-                          cells: <DataCell>[
-                            DataCell(Text('Qiyam')),
-                            DataCell(Text(convertdate12(snapshot.data.imsak))),
-                            DataCell(Text(convertdate(snapshot.data.imsak))),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ],
+                          DataRow(
+                            cells: <DataCell>[
+                              DataCell(Text('Dhur')),
+                              DataCell(Text(convertdate12(snapshot.data.dhur))),
+                              DataCell(Text(convertdate(snapshot.data.dhur))),
+                            ],
+                          ),
+                          DataRow(
+                            cells: <DataCell>[
+                              DataCell(Text('Asr')),
+                              DataCell(Text(convertdate12(snapshot.data.asr))),
+                              DataCell(Text(convertdate(snapshot.data.asr))),
+                            ],
+                          ),
+                          DataRow(
+                            cells: <DataCell>[
+                              DataCell(Text('Maghrib')),
+                              DataCell(
+                                  Text(convertdate12(snapshot.data.maghrib))),
+                              DataCell(
+                                  Text(convertdate(snapshot.data.maghrib))),
+                            ],
+                          ),
+                          DataRow(
+                            cells: <DataCell>[
+                              DataCell(Text('Isha')),
+                              DataCell(Text(convertdate12(snapshot.data.isha))),
+                              DataCell(Text(convertdate(snapshot.data.isha))),
+                            ],
+                          ),
+                          DataRow(
+                            cells: <DataCell>[
+                              DataCell(Text('Qiyam')),
+                              DataCell(
+                                  Text(convertdate12(snapshot.data.imsak))),
+                              DataCell(Text(convertdate(snapshot.data.imsak))),
+                            ],
+                          ),
+                        ],
+                      ),
+                      Divider(
+                        height: 10,
+                        thickness: 3,
+                      ),
+                      ListTile(
+                        leading: Icon(CupertinoIcons.sun_min),
+                        title: Text("Morning Zikr"),
+                        subtitle: Text(zikr(snapshot.data.fajr)),
+                      ),
+                      Divider(
+                        height: 10,
+                        thickness: 3,
+                      ),
+                      ListTile(
+                        leading: Icon(CupertinoIcons.moon_zzz),
+                        title: Text("Evening Zikr"),
+                        subtitle: Text(zikr(snapshot.data.asr)),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ],
-          );
+              ],
+            ),
+          ]);
         } else if (snapshot.hasError) {
           return Text("${snapshot.error}");
         }
@@ -248,13 +274,19 @@ class _Home extends State<Home> {
     return newdate.toString();
   }
 
-  convertdate12(String d){
+  convertdate12(String d) {
     var date = DateTime.parse("2020-01-20 " + d);
     var newdate = DateFormat("h:mma").format(date);
-    
 
     return newdate.toString();
+  }
 
+  zikr(String d) {
+    var date = DateTime.parse("2020-01-20 " + d);
+    var pdate = date.add(Duration(minutes: 30));
+    var newdate = DateFormat("h:mma").format(pdate);
+
+    return newdate.toString();
   }
 
   /*  nextprayer(String fajr, String dhur, String asr, String magrib, String isha){
